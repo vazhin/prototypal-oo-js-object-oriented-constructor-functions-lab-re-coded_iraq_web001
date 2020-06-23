6ad2a77846d5908e66472acf6025d6ba33398193
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-23 02:46:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model; 
+}
+
+function Driver(name, age, experience) {
+  this.year = year;
+  this.color = color;
+  this.model = model; 
+}
+
